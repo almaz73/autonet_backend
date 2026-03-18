e42c78a86923075d75350b538c86e01b60e38b4e
almaz73 <almaz_73@mail.ru>
2026-03-18 07:38:02 +0700

    сообщение об обновлениях в телеграм

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const Version = '18.03.2026, ver.1.5.0'
+export const Version = '18.03.2026, ver.1.6.1 telegram'
 export const FolderPhoto = '../front/pub_auto'
 export const FolderXML = '../front/XML'
--- src/startWorker.js
+++ src/startWorker.js
@@ -1,5 +1,16 @@
 import {Version} from "./constants.js";
 let report = `\n\nОтчет ${Version}`
 
+function send(message) {
+    let botId = 'bot8235288635:AAF_soJaYR8OPHAQrpfcF4FDUr2JjRRDlVw';
+    let chatId = '-5064627941';
+    let linkTelega = `https://api.telegram.org/${botId}/sendMessage?chat_id=${chatId}&parse_mode=HTML&text=${message}`;
 
+    fetch(linkTelega)
+        .then(response => response.json())
+        .then(() => {
+            console.log('сообщение доставлено = ')
+        });
+}
+send(report)
 console.log(report)
