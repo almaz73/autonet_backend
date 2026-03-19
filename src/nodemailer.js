c2463552d460e1e74968cc26c92cfda68534aa4a
almaz73 <almaz_73@mail.ru>
2026-03-19 16:53:32 +0700

    ip post

--- src/nodemailer.js
+++ src/nodemailer.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-    host: 'mx.orange.local',
+    host: '213.159.206.196',//'mx.orange.local',
     port: 587,
     auth: {
         user: 'autoset_info@cartat.ru',
